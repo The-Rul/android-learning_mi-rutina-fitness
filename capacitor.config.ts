728486ded9c5952f4627717cb93e2b75645df633
The-Rul <rul.univer@gmail.com>
2026-05-06 11:31:50 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'android-learning_mi-rutina-fitness',
+  webDir: 'www'
+};
+
+export default config;
