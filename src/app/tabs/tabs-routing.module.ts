547559438d0df00c7057a0108b97a84792af2757
The-Rul <rul.univer@gmail.com>
2026-05-06 12:03:36 +0200

    Navegacion y configuración principal de pestañas (tabs)
    Se configuran las pestañas principales de la aplicación manteniendo la estructura generada por Ionic y adaptando las rutas y etiquetas visibles a Rutinas, Añadir Ejercicio y Configuración.

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -8,27 +8,27 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'rutinas',
         loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
       },
       {
-        path: 'tab2',
+        path: 'add-ejercicio',
         loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
       },
       {
-        path: 'tab3',
+        path: 'configuracion',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/rutinas',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/rutinas',
     pathMatch: 'full'
   }
 ];
